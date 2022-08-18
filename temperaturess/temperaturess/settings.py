6f2aad0ceaedd5a2e680b3d9c32044fccffa2855
ZYZYSK <huangtianhe1423x@gmail.com>
2022-08-19 00:47:17 +0900

    fixed bugs

--- temperaturess/temperaturess/settings.py
+++ temperaturess/temperaturess/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l=++#_s^=v9)wk0(!$k_!^!#g_%6ier#ryw8zy^!a(!66cwo7y'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
