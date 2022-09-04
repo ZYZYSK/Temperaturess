c01b509ba4ab30f68619d0b3ddc9dff631ed6bde
ZYZYSK <huangtianhe1423x@gmail.com>
2022-09-04 11:38:51 +0900

    fix crontab.txt

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
-cd
+# copy files
+cp -r Temperaturess /opt
+cd /opt/Temperaturess
+echo "current directory: "+`pwd`
+
 # create virtualenv
 pip install virtualenv
-python -m virtualenv venv_tenoeraturess
+python -m virtualenv venv_temperaturess
+# enable virtualenv
+source venv_temperaturess/bin/activate
+# install libraries
+sudo apt install libglib2.0-dev
+pip install --upgrade pip
+pip install numpy
+pip install plotly
+pip install matplotlib
+pip install git+https://github.com/AmbientDataInc/ambient-python-lib.git
+pip install bluepy
+pip install uwsgi
+
+# crontab
