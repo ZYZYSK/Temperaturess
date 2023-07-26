a8efeda1db74daa06851fdbcffd8235105c2c543
prescott <huangtianhe1423x@gmail.com>
2023-07-26 12:57:27 +0900

    平均値の計算方法を修正

--- temperaturess/viewer/management/commands/inkbird.py
+++ temperaturess/viewer/management/commands/inkbird.py
@@ -24,16 +24,27 @@ def create_log(is_debug: bool = False) -> logging.Logger:
     # create directory "logs" if not exists
     os.makedirs("logs", exist_ok=True)
     # set save log path
-    log_handler = logging.FileHandler(datetime.datetime.now().strftime("logs/%Y_%m_%d_%H_%M_%S.log"))
+    log_handler = logging.FileHandler(
+        datetime.datetime.now().strftime("logs/%Y_%m_%d_%H_%M_%S.log")
+    )
     # set log format
-    log_format = logging.Formatter(fmt="%(asctime)s %(levelname)s %(filename)s %(funcName)s  %(lineno)s : %(message)s")
+    log_format = logging.Formatter(
+        fmt="%(asctime)s %(levelname)s %(filename)s %(funcName)s  %(lineno)s : %(message)s"
+    )
     log_handler.setFormatter(log_format)
     logger.addHandler(log_handler)
     return logger
 
 
 class Inkbird:
-    def __init__(self, address: str, handle: str, channel_id: str, write_key: str, logger: logging.Logger) -> None:
+    def __init__(
+        self,
+        address: str,
+        handle: str,
+        channel_id: str,
+        write_key: str,
+        logger: logging.Logger,
+    ) -> None:
         # MAC address
         self.address = address
         # handle
@@ -44,12 +55,15 @@ class Inkbird:
         # Log
         self.logger = logger
         # for debugging
-        self.logger.debug(f"MAC address: {self.address}, handle: {self.handle}, Channel ID: {self.channel_id}, write_key: {self.write_key}.")
+        self.logger.debug(
+            f"MAC address: {self.address}, handle: {self.handle}, Channel ID: {self.channel_id}, write_key: {self.write_key}."
+        )
 
     def get_data(self) -> None:
         # get current time
         self.tm = timezone.datetime.now()
         self.tm = timezone.make_aware(self.tm)
+        self.tm = self.tm.replace(minute=5 * (self.tm.minute // 5))
         # peripheral
         self.peripheral = btle.Peripheral(self.address)
         # read data
@@ -75,29 +89,44 @@ class Inkbird:
         # TimeData
         try:
             # save data to database
-            TimeData.objects.create(tm=self.tm, temperature=self.temperature, humidity=self.humidity, is_external=self.is_external)
+            TimeData.objects.create(
+                tm=self.tm,
+                temperature=self.temperature,
+                humidity=self.humidity,
+                is_external=self.is_external,
+            )
         # exception: Data Duplication
         except IntegrityError:
             self.logger.error("Error: This TimeData already exists!")
         # exception: Others
-        except Exception as e:
+        except Exception:
             self.logger.error(traceback.format_exc())
         # for debugging
         else:
-            self.logger.debug(f"Successfully created TimeData ({self.tm}, {self.temperature}C, {self.humidity}%).")
+            self.logger.debug(
+                f"Successfully created TimeData ({self.tm}, {self.temperature}C, {self.humidity}%)."
+            )
         # DayData
+        # if True:
         if self.tm.hour == 0 and self.tm.minute == 0:
             try:
                 # 作成
                 day = datetime.datetime.date(self.tm - datetime.timedelta(days=1))
-                timedatas = TimeData.objects.filter(tm__year=day.year, tm__month=day.month, tm__day=day.day)
+                # day = day.replace(year=2023, month=7, day=24)
+                timedatas = TimeData.objects.filter(
+                    tm__year=day.year, tm__month=day.month, tm__day=day.day
+                )
                 datas = timedatas.values()
 
                 # get min & max temperature
                 temperature_min = min([data.get("temperature") for data in datas])
                 temperature_max = max([data.get("temperature") for data in datas])
-                timedata_temperature_min = timedatas.filter(temperature=temperature_min).first()
-                timedata_temperature_max = timedatas.filter(temperature=temperature_max).first()
+                timedata_temperature_min = timedatas.filter(
+                    temperature=temperature_min
+                ).first()
+                timedata_temperature_max = timedatas.filter(
+                    temperature=temperature_max
+                ).first()
                 # get min & max humidity
                 humidity_min = min([data.get("humidity") for data in datas])
                 humidity_max = max([data.get("humidity") for data in datas])
@@ -108,22 +137,54 @@ class Inkbird:
                 time_past = datetime.datetime(day.year, day.month, day.day, 0, 0)
                 temperature_sum = np.sum([data.get("temperature") for data in datas])
                 humidity_sum = np.sum([data.get("humidity") for data in datas])
-                temperature_temp = np.mean([data.get("temperature") for data in datas])
-                humidity_temp = np.mean([data.get("humidity") for data in datas])
-                for _ in range(288):
-                    timedata_temp = list(filter(lambda item: item["tm"].hour == time_past.hour and item["tm"].minute == time_past.minute, datas))
+                temperature_temp0 = np.mean([data.get("temperature") for data in datas])
+                humidity_temp0 = np.mean([data.get("humidity") for data in datas])
+                cnt_errors = 0
+                for index in range(288):
+                    timedata_temp = list(
+                        filter(
+                            lambda item: item["tm"].hour == time_past.hour
+                            and item["tm"].minute == time_past.minute,
+                            datas,
+                        )
+                    )
                     # record the data temporary if exists
                     if len(timedata_temp) > 0:
-                        temperature_temp = timedata_temp[0]["temperature"]
-                        humidity_temp = timedata_temp[0]["humidity"]
+                        temperature_temp1 = timedata_temp[0]["temperature"]
+                        humidity_temp1 = timedata_temp[0]["humidity"]
+                        if cnt_errors > 0:
+                            temperature_sum += (
+                                cnt_errors * (temperature_temp0 + temperature_temp1) / 2
+                            )
+                            humidity_sum += (
+                                cnt_errors * (humidity_temp0 + humidity_temp1) / 2
+                            )
+                            cnt_errors = 0
+                        temperature_temp0 = temperature_temp1
+                        humidity_temp0 = humidity_temp1
                     # use the previous data if doesn't exist
                     else:
-                        temperature_sum += temperature_temp
-                        humidity_sum += humidity_temp
+                        cnt_errors += 1
+                        # if it is the last timedata in that day
+                        if index == 288:
+                            temperature_temp1 = np.mean(
+                                [data.get("temperature") for data in datas]
+                            )
+                            humidity_temp1 = np.mean(
+                                [data.get("humidity") for data in datas]
+                            )
+                            temperature_sum += (
+                                cnt_errors * (temperature_temp0 + temperature_temp1) / 2
+                            )
+                            humidity_sum += (
+                                cnt_errors * (humidity_temp0 + humidity_temp1) / 2
+                            )
+                            cnt_errors = 0
+
                     # advance the time
                     time_past += datetime.timedelta(minutes=5)
                 # check if all datas were recorded
-                is_incomplete = (len(datas) != 288)
+                is_incomplete = len(datas) != 288
                 # register data into database
                 DayData.objects.create(
                     day=day,
@@ -133,11 +194,14 @@ class Inkbird:
                     humidity_min=timedata_humidity_min,
                     humidity_max=timedata_humidity_max,
                     humidity_avg=humidity_sum / 288,
-                    is_incomplete=is_incomplete)
+                    is_incomplete=is_incomplete,
+                )
             # exception: Data Duplication
             except IntegrityError:
                 self.logger.warning("Warning: This DayData already exists!")
-                daydata = DayData.objects.filter(day__year=day.year, day__month=day.month, day__day=day.day).first()
+                daydata = DayData.objects.filter(
+                    day__year=day.year, day__month=day.month, day__day=day.day
+                ).first()
                 daydata.day = day
                 daydata.temperature_min = timedata_temperature_min
                 daydata.temperature_max = timedata_temperature_max
@@ -148,13 +212,17 @@ class Inkbird:
                 daydata.is_incomplete = is_incomplete
                 daydata.save()
                 # for debugging
-                self.logger.debug(f"Successfully created DayData ({day}, {temperature_min}C, {temperature_max}C, {temperature_sum/288}C,{humidity_min}%, {humidity_max}%, {humidity_sum/288}%).")
+                self.logger.debug(
+                    f"Successfully created DayData ({day}, {temperature_min}C, {temperature_max}C, {temperature_sum/288}C,{humidity_min}%, {humidity_max}%, {humidity_sum/288}%)."
+                )
             # exception: Others
             except Exception as e:
                 self.logger.error(traceback.format_exc())
             # for debugging
             else:
-                self.logger.debug(f"Successfully created DayData ({day}, {temperature_min}C, {temperature_max}C, {temperature_sum/288}C,{humidity_min}%, {humidity_max}%, {humidity_sum/288}%).")
+                self.logger.debug(
+                    f"Successfully created DayData ({day}, {temperature_min}C, {temperature_max}C, {temperature_sum/288}C,{humidity_min}%, {humidity_max}%, {humidity_sum/288}%)."
+                )
 
     def upload_ambient(self):
         # upload data to ambient
--- temperaturess/viewer/templatetags/timedata.py
+++ temperaturess/viewer/templatetags/timedata.py
@@ -1,59 +1,79 @@
 from django import template
 import numpy as np
+from ..models import DayData
+
 register = template.Library()
 
 
-@register.filter
+@register.simple_tag
 # 気温の平均値
-def average_temperature(datas: list):
-    if len(datas) == 0: return
-    temperatures = [data["temperature"] for data in datas]
-    return np.average(temperatures)
+def average_temperature(year: int, month: int, day: int, datas: list):
+    try:
+        return DayData.objects.get(
+            day__year=year, day__month=month, day__day=day
+        ).temperature_avg
+    except Exception as e:
+        print(e)
+        if len(datas) == 0:
+            return -1
+        temperatures = [data["temperature"] for data in datas]
+        return np.average(temperatures)
 
 
-@register.filter
+@register.simple_tag
 # 湿度の平均値
-def average_humidity(datas: list):
-    if len(datas) == 0: return
-    humidities = [data["humidity"] for data in datas]
-    return np.average(humidities)
+def average_humidity(year: int, month: int, day: int, datas: list):
+    try:
+        return DayData.objects.get(
+            day__year=year, day__month=month, day__day=day
+        ).humidity_avg
+    except Exception as e:
+        print(e)
+        if len(datas) == 0:
+            return -1
+        humidities = [data["humidity"] for data in datas]
+        return np.average(humidities)
 
 
 @register.filter
 # 気温の最小値と時間
 def min_temperature(datas: list, i=1):
-    if len(datas) == 0: return
+    if len(datas) == 0:
+        return
     if i == 1:
-        return min(datas, key=lambda x: x['temperature'])['temperature']
+        return min(datas, key=lambda x: x["temperature"])["temperature"]
     else:
-        return min(datas, key=lambda x: x['temperature'])['tm']
+        return min(datas, key=lambda x: x["temperature"])["tm"]
 
 
 @register.filter
 # 湿度の最小値と時間
 def min_humidity(datas: list, i=1):
-    if len(datas) == 0: return
+    if len(datas) == 0:
+        return
     if i == 1:
-        return min(datas, key=lambda x: x['humidity'])['humidity']
+        return min(datas, key=lambda x: x["humidity"])["humidity"]
     else:
-        return min(datas, key=lambda x: x['humidity'])['tm']
+        return min(datas, key=lambda x: x["humidity"])["tm"]
 
 
 @register.filter
 # 気温の最大値と時間
 def max_temperature(datas: list, i=1):
-    if len(datas) == 0: return
+    if len(datas) == 0:
+        return
     if i == 1:
-        return max(datas, key=lambda x: x['temperature'])['temperature']
+        return max(datas, key=lambda x: x["temperature"])["temperature"]
     else:
-        return max(datas, key=lambda x: x['temperature'])['tm']
+        return max(datas, key=lambda x: x["temperature"])["tm"]
 
 
 @register.filter
 # 湿度の最大値と時間
 def max_humidity(datas: list, i=1):
-    if len(datas) == 0: return
+    if len(datas) == 0:
+        return
     if i == 1:
-        return max(datas, key=lambda x: x['humidity'])['humidity']
+        return max(datas, key=lambda x: x["humidity"])["humidity"]
     else:
-        return max(datas, key=lambda x: x['humidity'])['tm']
+        return max(datas, key=lambda x: x["humidity"])["tm"]
